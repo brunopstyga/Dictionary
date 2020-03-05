c94a6de6c954703ed4b4ef282d6a0677467f0ddf
bruno pstyga <bruno.pstyga@followoz.com>
2020-03-05 14:30:01 -0300

    add send file to list data by gmail

--- app/src/main/java/com/example/practicaskotlin/presentation/view/ListFragment.kt
+++ app/src/main/java/com/example/practicaskotlin/presentation/view/ListFragment.kt
@@ -1,27 +1,28 @@
 package com.example.practicaskotlin.presentation.view
 
+import android.R.attr.path
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import android.os.Environment
+import android.view.*
+import androidx.core.content.FileProvider
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.practicaskotlin.BuildConfig
 import com.example.practicaskotlin.R
 import com.example.practicaskotlin.business.viewmodel.DataViewModel
+import com.example.practicaskotlin.data.model.room.Data
 import dagger.android.support.DaggerFragment
-import kotlinx.android.synthetic.main.fragment_list.*
-import java.lang.Exception
+import kotlinx.android.synthetic.main.fragment_list.view.*
+import java.io.File
 import javax.inject.Inject
-import kotlin.math.log
+
 
 class ListFragment : DaggerFragment() {
 
@@ -30,6 +31,12 @@ class ListFragment : DaggerFragment() {
     private lateinit var dataViewModel: DataViewModel
     private var recyclerView: RecyclerView? = null
     private lateinit var adapter: DatadListAdapter
+    private var listData = listOf<Data>()
+    private lateinit var imageUri : Uri
+    private lateinit var imgPath : String
+
+
+
 
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -39,15 +46,18 @@ class ListFragment : DaggerFragment() {
         } ?: throw Exception("Invalid Activity")
         dataViewModel.dataGet().observe(this, Observer {
             adapter.setData(it)
+            writeDataUri(it)
+
         })
         enableSwipe()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        toolbarList.setNavigationIcon(R.drawable.ic_back)
-        toolbarList.setTitle(R.string.app_name)
-        toolbarList.setOnClickListener {
+        view.textViewSend.setOnClickListener {
+            sendFile(imageUri)
+        }
+        view.textViewBack.setOnClickListener {
             removeFragment()
         }
 
@@ -61,8 +71,6 @@ class ListFragment : DaggerFragment() {
 
         var view = inflater.inflate(R.layout.fragment_list, container, false)
         val activity = activity as Context
-
-//        val model : DataViewModel by activityViewModels<DataViewModel>()
         recyclerView = view.findViewById<RecyclerView>(R.id.customRecyclerView)
         adapter = DatadListAdapter(activity) { id, newWord ->
             dataViewModel.editWords(id, newWord)
@@ -77,7 +85,6 @@ class ListFragment : DaggerFragment() {
         fun newInstance() =
             ListFragment()
     }
-
 
     private fun enableSwipe() {
         val simpleItemTouchCallback =
@@ -107,6 +114,41 @@ class ListFragment : DaggerFragment() {
         itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 
+    private fun writeDataUri(data: List<Data>): Uri {
+        val folder = File("${context!!.getExternalFilesDir(Environment.DIRECTORY_DCIM)}")
+        folder.mkdirs()
+
+        val file = File(folder, "DataList.txt")
+        if (file.exists())
+            file.delete()
+        file.createNewFile()
+            imageUri = FileProvider.getUriForFile(
+                activity!!,
+                BuildConfig.APPLICATION_ID + getString(R.string.file_provider_name),
+                file
+            )
+//        /storage/emulated/0/Android/data/com.example.practicaskotlin/files/DCIM/DataList.txt
+        imgPath = file.absolutePath
+        file.printWriter().use {
+            out ->
+            data.forEach {
+                out.println("${it.dataEnglish} -----> ${it.dataSpanish}")
+            }
+        }
+
+        return imageUri
+    }
+
+    private fun sendFile(file: Uri){
+        val emailIntent = Intent(Intent.ACTION_SEND)
+        emailIntent.type = "vnd.android.cursor.dir/email"
+        val to = arrayOf("bpstyga@gmail.com")
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, to)
+        emailIntent.putExtra(Intent.EXTRA_STREAM, file)
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject")
+        startActivity(Intent.createChooser(emailIntent, "Send email..."))
+    }
+
     fun removeFragment(){
         val manager = activity?.supportFragmentManager
         val listFragment = manager?.findFragmentByTag("ListFragment")
@@ -115,6 +157,14 @@ class ListFragment : DaggerFragment() {
         transaction?.commit()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        return inflater.inflate(R.menu.menu, menu)
+    }
 
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
